1c7ce5e509619bd485e06bfe503dead80d30e03b
enzonuma <enzo.nmateluna@gmail.com>
2022-06-14 01:11:37 -0400

    Arreglo de concatenación de arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,30 +1,60 @@
 let welcome = alert("Welcome to Xclusive Shoes!")
 let myShoes = []
+let myClothes = []
+let myAcce = []
 
 function crearId() {
     return parseInt(Math.random() * 100)
 }
 
 function congrats() {
-    congratstext = prompt("Your shoes were added! Do you want to add another ones? (Y / N to close")
+    congratstext = prompt("Your products were added succesfully! Do you want to add another ones? (Y / N to close")
 }
 
-function addShoes() {
+function addProduct() {
     class Producto {
-        constructor(sku, shoesName, minBid) {
+        constructor(sku, productType, productName, minBid) {
             this.sku = sku
-            this.shoesName = shoesName
+            this.productName = productName
             this.minBid = parseInt(minBid)
+            this.productType = productType
         }
     }
 
-    shoesName = prompt("Enter the shoes Model/Name:")
-    minBid = prompt("Enter the min. $Bid:")
-    sku = crearId()
-    myShoes.push(new Producto(sku, shoesName, minBid))
+    productType = prompt("Do you want to add: Shoe | Clothes | Accesories ?")
+
+    // debugger
+
+    switch (productType) {
+
+        case "Shoe":
+            productName = prompt("Enter the shoes Model/Name:");
+            minBid = prompt("Enter the min. $Bid:");
+            sku = crearId();
+            myShoes.push(new Producto(sku, productType, productName, minBid));
+            break;
+
+        case "Clothes":
+            productName = prompt("Enter the Clothes Model/Name:");
+            minBid = prompt("Enter the min. $Bid:");
+            sku = crearId();
+            myClothes.push(new Producto(sku, productType, productName, minBid));
+            break;
+
+        case "Accesories":
+            productName = prompt("Enter the Accesorie Model/Name:");
+            minBid = prompt("Enter the min. $Bid:");
+            sku = crearId();
+            myAcce.push(new Producto(sku, productType, productName, minBid));
+            break;
+
+        default :
+            alert("Enter a correct category");
+            return addProduct();
+    }
 }
 
-addShoes()
+addProduct()
 
 congrats()
 
@@ -32,18 +62,19 @@ while (congratstext != "N") {
 
     switch (congratstext) {
         case "Y" :
-            addShoes();
+            addProduct();
             congrats();
             break;
 
         default :
             alert("Please enter an option");
-            congrats = prompt("Your shoes were added. Do you want to add another ones? (Y / N to close");
-            addShoes();
+            congrats = prompt("Your products were added! Do you want to add another ones? (Y / N to close");
+            addProduct();
             break;
     }
 
 } 
 
-let thankYou = alert("Thank you for visit us! Here are your Shoes, good luck!")
-console.table(myShoes)
+let thankYou = alert("Thank you for visit us! Here are your products, good luck!")
+const allProducts = myShoes.concat(myClothes, myAcce)
+console.table(allProducts)
